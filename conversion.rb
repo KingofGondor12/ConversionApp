43839091f871c266dd4d78738419572ed6f560eb
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-10-03 14:23:06 +1000

    Added infinite base conversions

--- conversion.rb
+++ conversion.rb
@@ -5,6 +5,22 @@ puts "Enter Base For Conversion:"
 base = $stdin.gets.chomp.capitalize
 if base == "Binary"
   binary_conversion
-elsif base == "Hexadecimal" || "Hex"
+elsif base == "Hexadecimal" || base == "Hex"
   hex_conversion
+elsif base != "Hexadecimal" || base == "Hex" || base == "Binary"
+  base = base.to_i
+  puts "Using Base #{base}"
+  puts "Do you wish to convert to or from #{base}?"
+  user_input = $stdin.gets.chomp.capitalize
+  if user_input == "To"
+    puts "Enter Integer To Convert:"
+    integer = $stdin.gets.chomp.to_i
+    integer = integer.to_s(base)
+    puts integer
+  elsif user_input == "From"
+    puts "Enter Integer To Convert:"
+    conversion = $stdin.gets.chomp.to_s
+    conversion = conversion.to_i(base)
+    puts conversion
+  end
 end
