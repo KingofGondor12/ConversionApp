7f58766044c40726aa40ffdc12a7391919447a17
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-10-03 14:39:45 +1000

    Added Base Limit + Prompt

--- conversion.rb
+++ conversion.rb
@@ -9,18 +9,22 @@ elsif base == "Hexadecimal" || base == "Hex"
   hex_conversion
 elsif base != "Hexadecimal" || base == "Hex" || base == "Binary"
   base = base.to_i
-  puts "Using Base #{base}"
-  puts "Do you wish to convert to or from #{base}?"
-  user_input = $stdin.gets.chomp.capitalize
-  if user_input == "To"
-    puts "Enter Integer To Convert:"
-    integer = $stdin.gets.chomp.to_i
-    integer = integer.to_s(base)
-    puts integer
-  elsif user_input == "From"
-    puts "Enter Integer To Convert:"
-    conversion = $stdin.gets.chomp.to_s
-    conversion = conversion.to_i(base)
-    puts conversion
+  if base > 36
+    puts "Enter A Base Between 1 and 36"
+  else
+    puts "Using Base #{base}"
+    puts "Do you wish to convert to or from #{base}?"
+    user_input = $stdin.gets.chomp.capitalize
+    if user_input == "To"
+      puts "Enter Integer To Convert:"
+      integer = $stdin.gets.chomp.to_i
+      integer = integer.to_s(base)
+      puts integer
+    elsif user_input == "From"
+      puts "Enter Integer To Convert:"
+      conversion = $stdin.gets.chomp.to_s
+      conversion = conversion.to_i(base)
+      puts conversion
+    end
   end
 end
