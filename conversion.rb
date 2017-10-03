10a0a6e44e09cb645bf0144faac72aaf2ea1d1a0
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-10-03 15:40:31 +1000

    Corrected if statements

--- conversion.rb
+++ conversion.rb
@@ -4,15 +4,17 @@ require_relative 'base_convert.rb'
 
 puts "Enter Base For Conversion:"
 base = $stdin.gets.chomp.capitalize
-if base == "Binary"
+if base == "Binary" || base == "2"
   binary_conversion
-elsif base == "Hexadecimal" || base == "Hex"
+elsif base == "Hexadecimal" || base == "Hex" || base =="16"
   hex_conversion
-elsif base != "Hexadecimal" || base == "Hex" || base == "Binary"
+elsif base != "Hexadecimal" || base != "Hex" || base != "Binary" || base != "2" || base !="16"
   @base = base.to_i
   if @base > 36
     puts "Enter A Base Between 1 and 36"
-  else
+  elsif @base <= 36 && @base > 0
     base_conversion
+  else
+    puts "Unknown Base. Please Enter A Base Between 1 and 36."
   end
 end
