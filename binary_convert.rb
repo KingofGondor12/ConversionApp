c71fd8ed62a44f1047e45930c305d6981afa011c
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-10-03 13:54:16 +1000

    .to_i workaround for custom .to_i method

--- binary_convert.rb
+++ binary_convert.rb
@@ -1,3 +1,6 @@
+def convert(str)
+  str.split('').reduce(0) { |m,x| 10 * (m + '0123456789'.index(x)) } / 10
+end
 def binary_conversion
   puts "Do you wish to convert to or from binary?"
   user_input = $stdin.gets.chomp.capitalize
@@ -12,7 +15,7 @@ def binary_conversion
     base = 1
     total = 0
     binary.split('').reverse.each do |digit|
-      total = total + (digit.to_i *  base)
+      total = total + (convert(digit) *  base)
       base = base * 2
     end
     puts total
